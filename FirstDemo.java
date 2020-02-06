8fa82efcc50c5446eadbe71c361c7621802a6f93
BMoumita <60728234+BMoumita@users.noreply.github.com>
2020-02-06 18:46:29 +0530

    Create object and Call methods

---
+++ FirstDemo.java
@@ -0,0 +1,9 @@
+public class FirstDemo {
+    void showData(){
+        System.out.print("Hello");
+    }
+    public static void main(String[] args){
+        FirstDemo demo = new FirstDemo();
+        demo.showData();
+    }
+}
