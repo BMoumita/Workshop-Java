2e3bf69472330703ffb2330ba08afa6e46ab55cc
BMoumita <60728234+BMoumita@users.noreply.github.com>
2020-02-06 14:47:06 +0530

    Interface

---
+++ abstraction/AbstractDemo.java
@@ -0,0 +1,12 @@
+package abstraction;
+
+public abstract class AbstractDemo {
+	abstract void showData(); //abstract method
+	void getData()      //non-abstract method
+	{
+		System.out.println("Abstract class");
+	}
+
+	
+
+}
---
+++ abstraction/CheckAbstraction.java
@@ -0,0 +1,18 @@
+package abstraction;
+
+public class CheckAbstraction extends AbstractDemo implements InterfaceDemo {
+	void showData() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CheckAbstraction abstraction = new CheckAbstraction();
+		abstraction.showData();
+
+	}
+	public void add() {
+		
+	}
+
+}
