228aaf29639d89f804963c1a8a250661775e1596
BMoumita <60728234+BMoumita@users.noreply.github.com>
2020-02-06 14:08:20 +0530

    Abstraction

---
+++ CheckAbstraction.java
@@ -0,0 +1,15 @@
+package abstraction;
+
+public class CheckAbstraction extends AbstractDemo {
+	void showData() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CheckAbstraction abstraction = new CheckAbstraction();
+		abstraction.showData();
+
+	}
+
+}
