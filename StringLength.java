d88a87ca2e9611f142a317e52f2764a50ea3e0d7
BMoumita <60728234+BMoumita@users.noreply.github.com>
2020-02-07 13:18:28 +0530

    Using String length() method

---
+++ StringLength.java
@@ -0,0 +1,12 @@
+package string;
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "DYPATILCOLLEGE";
+		System.out.println(s.length());
+
+	}
+
+}
