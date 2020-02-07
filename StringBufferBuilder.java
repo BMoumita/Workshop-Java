edcb809b5c1cbabb3df87e83ab639376d3fbc7af
BMoumita <60728234+BMoumita@users.noreply.github.com>
2020-02-07 13:14:59 +0530

    String Buffer, String Builder

---
+++ StringBufferBuilder.java
@@ -0,0 +1,16 @@
+package string;
+
+public class StringBufferBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer buffer = new StringBuffer("Java");
+		StringBuilder builder = new StringBuilder("Java");
+		
+		System.out.println(buffer.append(" Training"));
+		System.out.println(builder.append(" Workshop"));
+		
+
+	}
+
+}
