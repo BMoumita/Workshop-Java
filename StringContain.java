6b58d6054f3f5286a0745ad10248cddebf4e547b
BMoumita <60728234+BMoumita@users.noreply.github.com>
2020-02-07 13:16:42 +0530

    Using String Contain() method

---
+++ StringContain.java
@@ -0,0 +1,13 @@
+package string;
+
+public class StringContain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Hello world";
+		System.out.println(s.contains("Hel"));  //true
+		System.out.println(s.contains("mor"));  //false
+
+	}
+
+}
