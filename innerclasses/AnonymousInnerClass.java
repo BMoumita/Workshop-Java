4fa61353853e2cd092008b8eaff0450623751aac
BMoumita <60728234+BMoumita@users.noreply.github.com>
2020-02-10 11:54:55 +0530

    anonymous inner class

---
+++ innerclasses/AnonymousInnerClass.java
@@ -0,0 +1,16 @@
+package innerclasses;
+import abstraction.AbstractDemo;
+
+public class AnonymousInnerClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AbstractDemo demo = new AbstractDemo() {
+		public void showData() {
+		} 
+		
+
+	 
+   };	
+}
+}
